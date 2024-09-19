b351879b01630b0d30459041c6bce3b66b827722
Anton Antonau <anton.antonau.mogilev@gmail.com>
2024-09-19 15:00:07 +0300

    fixes cell colors

--- src/cell.js
+++ src/cell.js
@@ -1,3 +1,17 @@
+const COLORS = {
+    2: '#fcefca',
+    4: '#fae3a2',
+    8: '#fad773',
+    16: '#faca3e',
+    32: '#fabf11',
+    64: '#c99806',
+    128: '#997405',
+    256: '#806104',
+    512: '#664f09',
+    1024: '#784904',
+    2028: '#783e04',
+}
+
 export class Cell {
     constructor(canvasBoard, elementNumber) {
         this.elementNumber = elementNumber;
@@ -29,13 +43,14 @@ export class Cell {
     }
 
     setColorCell(value) {
-        this.ctx.fillStyle = `rgb(${Math.floor(255 - value / 9)}, ${Math.floor(255 - value / 9)}, 0)`;
+        //this.ctx.fillStyle = `rgb(${Math.floor(255 - value / 9)}, ${Math.floor(255 - value / 9)}, 0)`;
+        this.ctx.fillStyle = COLORS[value];
         this.ctx.fillRect(0, 0, 300, 150);
     };
 
     setValueCell(value) {
         this.ctx.fillStyle = "black";
-        this.ctx.font = "bold 56px sans-serif";
+        this.ctx.font = "bold 60px sans-serif";
         this.ctx.textAlign = "center";
         this.ctx.textBaseline = "middle";
         this.ctx.fillText(value, this.cell.height, this.cell.width/4)
